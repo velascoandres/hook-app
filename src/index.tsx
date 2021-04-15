c2ca16e7528e6e8b2866a7b8de196b59f58d730a
Andrés Velasco <velasco.andrs@gmail.com>
2021-04-14 23:30:46 -0500

    tarea memorize

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Padre } from './components/07-tarea-memo/Padre';
 import { HookApp } from './HookApp';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/*
     <HookApp />
+    */}
+   <Padre /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
