6e480b9944bb13a0a41baabb1d5c93c125f29b5a
Andrés Velasco <velasco.andrs@gmail.com>
2021-04-22 04:03:41 -0500

    todo skeleton

--- src/components/08-useReducer/TodoApp.tsx
+++ src/components/08-useReducer/TodoApp.tsx
@@ -9,19 +9,57 @@ export const TodoApp = () => {
 
     const [todos] = useReducer(todoReducer, initialState);
 
-    console.log(todos);
+
+
+    const renderizarTodos = () => {
+        return todos.map(
+            ({ id, desc }, indice: number) => (
+                <li
+                    key={id}
+                    className="list-group-item"
+                >
+                    <p className="text-center">
+                        {indice + 1}. {desc}
+                    </p>
+                    <button
+                        className="btn btn-danger"
+                    >
+                        Borrar
+                    </button>
+                </li>
+            ),
+        );
+    };
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length})</h1>
             <hr />
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {renderizarTodos()}
+                    </ul>                </div>
+                <div className="col-5">
+                    <h4>Agregar Todo</h4>
+                    <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
-
+                    <form>
+                        <input
+                            type="text"
+                            name="Description"
+                            placeholder="Aprender ..."
+                            autoComplete="off"
+                            className="form-control"
+                        />
+                        <button
+                            className="btn btn-outline-primary mt-1 btn-block"
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
