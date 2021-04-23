25e5aa118ef1b283c3b39614986702a15bfe2216
andres velasco <velasco.andrs@gmail.com>
2021-04-23 13:00:35 -0500

    setup tests

---
+++ src/test/HookApp.test.tsx
@@ -0,0 +1,10 @@
+import { shallow } from 'enzyme';
+import {HookApp} from '../HookApp';
+
+describe('Pruebas en <HookApp />', () => {
+   test('Debe mostrarse correctamente', () => {
+       const wrapper = shallow( <HookApp />);
+       expect(wrapper).toMatchSnapshot();
+   });
+   
+});
